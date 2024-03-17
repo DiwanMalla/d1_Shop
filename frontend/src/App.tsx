d389111bc9a3bf07c8ac763610f7bfe3abe6f94d
Diwan Malla <diwanmalla@Diwans-MacBook-Air.local>
2024-03-17 18:00:18 +1100

    first commit after another repo crsh

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import Banner from "./components/Banner/Banner";
+import Category from "./components/Category/Category";
+import Category2 from "./components/Category/Category2";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Services from "./components/Services/Services";
+import headphone from "./assets/hero/headphone.png";
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4: "Lorem ipsum lorem ipsum,ipsum lorem ipsum ipsum lorem ipsum",
+  bgColor: "#f42c37",
+};
+const App = () => {
+  return (
+    <div className="bg-whiteb dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
+      <Navbar />
+      <Hero />
+      <Category />
+      <Category2 />
+      <Services />
+      <Banner data={BannerData} />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Banner/Banner.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Button from "../Shared/Button";
+interface BannerData {
+  bgColor: string;
+  discount: string;
+  title: string;
+  date: string;
+  image: string;
+  title2: string;
+  title3: string;
+  title4: string;
+}
+interface BannerProps {
+  data: BannerData;
+}
+
+const Banner: React.FC<BannerProps> = ({ data }) => {
+  return (
+    <div className="min-h-[550px] flex justify-center items-start py-12">
+      <div className="container">
+        <div
+          style={{ background: data.bgColor }}
+          className="grid grid-cols-1 md:grid-cols-3 gap-6 items-center text-white rounded-3xl"
+        >
+          {/* first col */}
+          <div className="p-6 sm:p-8">
+            <p className="text-sm">{data.discount}</p>
+            <h1 className="uppercase text-4xl lg:text-7xl font-bold">
+              {data.title}
+            </h1>
+            <p className="text-sm">{data.date}</p>
+          </div>
+          {/* second col */}
+          <div className="hover:scale-150 duration-300 h-full flex items-center">
+            <img
+              src={data.image}
+              alt=""
+              className="scale-125 w-[250px] md:w-[340px] mx-auto drop-shadow-2xl object-cover"
+            />
+          </div>
+          {/* third col */}
+          <div className="flex flex-col justify-center gap-4 p-6 sm:p-8">
+            <p className="font-bold text-xl">{data.title2}</p>
+            <p className="text-3xl sm:text-5xl font-bold">{data.title3}</p>
+            <p className="text-sm tracking-wide leading-5">{data.title4}</p>
+            <div>
+              <Button
+                text="Shop Now"
+                bgColor="bg-white"
+                textColor="text-primary"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
